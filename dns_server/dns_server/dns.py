0e2e62d6adad3e16d5ff335406d400afa3c3c395
Alexandr Chudopal <alex.ch@core-tech.ru>
2021-02-21 11:08:26 +0300

    added class for parse dns message

--- dns_server/dns_server/dns.py
+++ dns_server/dns_server/dns.py
@@ -6,6 +6,36 @@ PORT = 53
 DNS_HOST = '8.8.8.8'
 DNS_PORT = 53
 
+
+class DNSQueryParser():
+    def __init__(self, dns_message):
+        self._message = dns_message
+
+    @property
+    def message(self):
+        return message
+
+    @property
+    def response(self):
+        self,_build_response()
+
+    def _build_response(self):
+        """Generate answer for dns query
+        """
+        # Transaction ID
+        TransactionID = self._get_transaction_id()
+
+    def _get_transaction_id(self) -> list:
+        """Returned id of dns message
+
+        Returns:
+            list: 2 bytes for transaction id
+        """
+        return self._message[:2]
+    
+
+
+
 def send_authority(message):
     server_address = (DNS_HOST, DNS_PORT)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
