5f725b7c69a4de9bd08d87632cd34657245c211a
Alexandr Chudopal <alex.ch@core-tech.ru>
2021-02-25 15:29:10 +0300

    added docks for servers

--- dns_server/dns_server/infrasructure/UDP_server.py
+++ dns_server/dns_server/infrasructure/UDP_server.py
@@ -1,15 +1,61 @@
 #from configuration import DNS_HOST, HOST
-import socket
-
 #HOST = '127.0.0.1'
 #PORT = 53
 #DNS_HOST = '8.8.8.8'
 #DNS_PORT = 53
 
-class UDPServer():
+#from dns_server.core.DNS_message import DNSMessage
+#import socket
+#import sys
+#import os
+#
+#PACKAGE_PARENT = '..'
+#SCRIPT_DIR = os.path.dirname(os.path.realpath(
+#    os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+#sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
-    def __init__(self, 
-                 host: str = '127.0.0.1', 
+import socket
+
+
+class DNSMessage():
+    """Entity for dns message
+    """
+
+    def __init__(self, dns_message):
+        self._message: bytearray = dns_message
+        self._responce: bytearray = None
+
+    def _build_response(self):
+        """Generate answer for dns query
+        """
+        # Transaction ID
+        transaction_id = self._get_transaction_id()
+
+    def _get_transaction_id(self) -> list:
+        """Returned id of dns message
+
+        Returns:
+            list: 2 bytes for transaction id
+        """
+        return self._message[:2]
+
+    @property
+    def message(self) -> bytearray:
+        return self._message
+
+    @property
+    def response(self):
+        self._build_response()
+
+
+
+
+class UDPServer():
+    """ UDP server for receiving and sending 
+    messages to DNS authority server
+    """
+    def __init__(self,
+                 host: str = '127.0.0.1',
                  port: int = 53,
                  dns_host: str = '8.8.8.8',
                  dns_port: int = 53):
@@ -19,6 +65,8 @@ class UDPServer():
         self._dns_port: int = dns_port
 
     def run(self):
+        """Initialize and run server execution
+        """
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self._socket.bind(
             (
@@ -31,13 +79,13 @@ class UDPServer():
     def _run_server(self):
         while(True):
             data, addr = self._socket.recvfrom(512)
-            
+
             print(data)
-            
+
             dns_response = self._send_authority(message=data)
-            
+
             self._socket.sendto(dns_response, addr)
-            
+
     def _send_authority(self, message):
         server_address = (self._dns_host, self._dns_port)
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -48,5 +96,8 @@ class UDPServer():
             sock.close()
         return data
 
-server = UDPServer('127.0.0.1', 53)
-server.run()
+
+
+if __name__ == "__main__":
+    server = UDPServer('127.0.0.1', 53)
+    server.run()
